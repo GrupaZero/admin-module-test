3e15ea2c59b97dcf2f7643621e37d58ea82c7b99
Bartero <bartek.krzywania@gmail.com>
2018-03-15 13:12:44 +0100

    A third party package can now register itself
    A developer using this third-party package doesn't have to know any details now

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 export default {
-  register: (router, menu) => {
-    return 'Registering Service Provider: Admin Module Test'
-  }
+    register: (app) => {
+        console.log('Registering Service Provider: Admin Module Test')
+
+        app.registerLauncher({
+            path: 'super-advanced-feature',
+            label: 'Super-Duper Advanced Feature'
+        }, resolve => require(['./lib/components/SuperAdvancedFeature.vue'], resolve))
+
+        app.registerLauncher({
+            path: 'another-super-advanced-feature',
+            label: 'Another Super-Duper Advanced Feature'
+        }, resolve => require(['./lib/components/AnotherSuperAdvancedFeature.vue'], resolve))
+    }
 }
